d2e2762118948df5083ca8569a7d0f7879cc9fe2
aryapraset <aryaprasetiyo12@gmail.com>
2023-02-10 11:51:39 +0800

    menambahkan fungsi getBooksByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -63,11 +63,36 @@ const addBooksHandler = (request, h) => {
   }).code(500)
 }
 
-const getAllBooksHandler = () => ({
-  status: 'success',
-  data: {
-    books: []
-  }
-}).code(200)
+const getAllBooksHandler = () => {
+  return ({
+    status: 'success',
+    data: {
+      books: []
+    }
+  }).code(200)
+}
 
-module.exports = { addBooksHandler, getAllBooksHandler }
+const getBooksByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const book = books.filter((n) => n.id === id)[0]
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book
+      }
+    }
+      .code(200)
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+module.exports = { addBooksHandler, getAllBooksHandler, getBooksByIdHandler }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
-const { addBooksHandler } = require('./handler')
+const { addBooksHandler, getAllBooksHandler, getBooksByIdHandler } = require('./handler')
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: addBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler
   }
+
 ]
 
 module.exports = routes
